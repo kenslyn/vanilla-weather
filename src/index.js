04de81e7b022b028f1f000f8a224b3aae0fcc66a
Mackenzie Agnir <121129438+kenslyn@users.noreply.github.com>
2024-02-02 14:36:15 -0700

    Change and updated index and css

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ function displayForecast(response) {
         `
   <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
-        
+
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
